59753dec9ae2c71398f120ae786d6df35d079729
shayan-soft <shayan.soft.solout@gmail.com>
2015-12-12 22:03:08 +0000

    Update Server.lua

--- vir/Server.lua
+++ vir/Server.lua
@@ -1,9 +1,21 @@
 local function run(msg)
-if msg.text == "!turn off" then
-	return "Shuting Down..."
+if msg.text == "[!/]turn off" then
+	return "Please Wait 8Min\nServer Shuting Down..."
 end
-if msg.text == "!server" then
+if msg.text == "[!/]server" then
 	return "https://212.33.207.97:2222"
+end
+if msg.text == "[!/]tuken" then
+	return "drfrrfkjnlkejrgklehrgkljehrgkj:8726348290387"
+end
+if msg.text == "[!/]login" then
+	return "https://umbrella.shayan-soft.ir:2222"
+end
+if msg.text == "[!/]reset" then
+	return "Are You Sure??"
+end
+if msg.text == "[!/]restart" then
+	return "Please Wait 8Min\nServer Restarting..."
 end
 end
 
@@ -11,10 +23,20 @@ return {
 	description = "Server Switch and Access", 
 	usage = {
 		"/turn off : turn off server",
-		"/server : access server" },
+		"/restart : restart server",
+		"/reset : delete database",
+        "/server : access server",
+		"/login : access server",
+		"/tuken : server tukrn",
+		},
 	patterns = {
 		"^[!/]turn? (off)",
-		"^[!/]server$"}, 
+		"^[!/]restart$",
+		"^[!/]reset$",
+		"^[!/]server$",
+		"^[!/]login$",
+		"^[!/]tuken$",
+		}, 
 	run = run,
     privileged = true,
 	pre_process = pre_process
