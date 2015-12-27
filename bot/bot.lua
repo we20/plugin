c0f1c173b3285e5e39a134d86ae63ba7416f4684
we20 <wez20@mihanmail.ir>
2015-12-27 10:28:14 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -263,7 +263,7 @@ function load_plugins()
 
     local ok, err =  pcall(function()
       local t = loadfile("vir/"..v..'.lua')()
-      plugins[v] = t
+      vir[v] = t
     end)
 
     if not ok then
